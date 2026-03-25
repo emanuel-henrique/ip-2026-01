2f048beb2c394f820b051d4dcf663e329a4b2aa8
Emanuel Henrique <emanuelhenrique1562@gmail.com>
2026-03-25 20:12:20 -0300

    Update Ex01.go

--- Aula06/Lista03/Ex01.go
+++ Aula06/Lista03/Ex01.go
@@ -1,1 +1,15 @@
+package main
+import "fmt"
 
+// IMPAR OU PAR?
+func main() {
+  var num int
+  fmt.Print("Digite o numero: ")
+  fmt.Scan(&num)
+  
+  if num%2 == 0 {
+    fmt.Printf("O número %d é par!", num)
+  } else{
+    fmt.Printf("O número %d é impar!", num) 
+  }
+}
