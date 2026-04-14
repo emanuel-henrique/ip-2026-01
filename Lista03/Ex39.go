d1e74fa1eabd8689b9d7a8a27b1110a49f31bc6d
Emanuel Henrique <emanuelhenrique1562@gmail.com>
2026-04-13 21:13:45 -0300

    Update Ex39.go

--- Lista03/Ex39.go
+++ Lista03/Ex39.go
@@ -3,13 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	var peso, maiorPeso float64
-	var numero, idMaiorPeso, qtd int
+	var peso, maiorPeso, menorPeso float64
+	var numero, idMaiorPeso, idMenorPeso, qtd int
 
 	fmt.Print("Quantidade de Bois: ")
 	fmt.Scan(&qtd)
 
 	for i := 1; i <= qtd; i++ {
+
 		fmt.Printf("Digite o Indentificador do %dº Boi: ", i)
 		fmt.Scan(&numero)
 
@@ -19,9 +20,20 @@ func main() {
 			maiorPeso = peso
 			idMaiorPeso = numero
 		}
+		if i == 1 {
+			menorPeso = peso
+		}
+		if peso < menorPeso {
+			menorPeso = peso
+			idMenorPeso = numero
+		}
+
 		fmt.Println("============================================")
 	}
 
 	fmt.Println("Identificador: ", idMaiorPeso)
-	fmt.Print("Maior peso: ", maiorPeso)
+	fmt.Println("Maior peso: ", maiorPeso)
+	fmt.Println("============================================")
+	fmt.Println("Identificador: ", idMenorPeso)
+	fmt.Println("Menor peso: ", menorPeso)
 }
