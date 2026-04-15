cdf88be1a694eeeca6679b5259dd76f99f33529a
Emanuel Henrique <emanuelhenrique1562@gmail.com>
2026-04-15 19:42:37 -0300

    Update Ex02.go

--- Aula15/Ex02.go
+++ Aula15/Ex02.go
@@ -1,1 +1,15 @@
+package main
+import "fmt"
 
+func main() {
+    nums := []int{10,20,36}
+    fmt.Println(somar(nums, len(nums)-1))
+}
+
+func somar(arr []int, n int) int {
+    if n == 0 {
+        return arr[n]
+    }   
+
+    return arr[n] + somar(arr, n-1)
+}
