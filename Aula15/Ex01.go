d426a6e167a7408ea8e16c2b6ffb7f76385479d6
Emanuel Henrique <emanuelhenrique1562@gmail.com>
2026-04-15 19:20:47 -0300

    Update Ex01.go

--- Aula15/Ex01.go
+++ Aula15/Ex01.go
@@ -1,1 +1,13 @@
+package main
+import "fmt"
 
+func main() {
+  fmt.Println(potenciacao(6,1))
+}
+
+func potenciacao(x,n int) int {
+    if n == 0{
+        return 1
+    } 
+    return x * potenciacao(x,n-1)
+}
