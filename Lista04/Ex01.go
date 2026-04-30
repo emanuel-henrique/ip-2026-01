b0d81e23d987c4377d65801d23ad0cf521fd18a7
Emanuel Henrique <emanuelhenrique1562@gmail.com>
2026-04-30 14:26:51 -0300

    Implement random number generation and position tracking
    Generate random numbers and track positions of those greater than 50.

--- Lista04/Ex01.go
+++ Lista04/Ex01.go
@@ -1,1 +1,30 @@
+package main
 
+import (
+	"fmt"
+	"math/rand/v2"
+)
+
+func main() {
+	count := 10
+	var nums []int
+	var positions []int
+	
+	for i := range count {
+		num := rand.IntN(100)
+		if num > 50{
+			positions = append(positions, i)
+		}
+		nums = append(nums, num)
+	}
+
+  fmt.Println("Array: ", nums)
+
+	if len(nums) > 0{
+		for _,n := range positions{
+			fmt.Printf("%d - Posição: %d\n", nums[n], n)
+		}
+	} else {
+			fmt.Print("Não há nenhum número superior à 50!")
+	}
+}
